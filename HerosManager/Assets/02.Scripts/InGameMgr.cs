a4c0dc5585080204c349d68edaf9f37dd59e135d
ybeom94 <ybeom94@nate.com>
2021-10-21 02:54:38 +0900

    CSV

---
+++ HerosManager/Assets/02.Scripts/InGameMgr.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameMgr : Singleton<InGameMgr>
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
