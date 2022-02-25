23fd9477db4e6aecbec7bd63d1a0a91c88667e47
ybeom94 <ybeom94@nate.com>
2022-02-25 13:36:40 +0900

    CSV

--- HerosManager/Assets/02.Scripts/Datas/LoadGameData.cs
+++ HerosManager/Assets/02.Scripts/Datas/LoadGameData.cs
@@ -5,6 +5,19 @@ using System.Text.RegularExpressions;
 
 public class LoadGameData : Singleton<LoadGameData>
 {
+    //public Dictionary<string, HeroStateData> heroStateDatas = new Dictionary<string, HeroStateData>();
+    //public Dictionary<string, ItemData> itemDatas = new Dictionary<string, ItemData>();
+    //public Dictionary<string, JourneyData> journeyDatas = new Dictionary<string, JourneyData>();
+    //public Dictionary<string, JRwithHeroData> jRwithHeroDatas = new Dictionary<string, JRwithHeroData>();
+    //public Dictionary<string, JStateData> jStateDatas = new Dictionary<string, JStateData>();
+    //public Dictionary<string, JVCutData> jVCutDatas = new Dictionary<string, JVCutData>();
+    //public Dictionary<string, JVwithObjectData> jVwithObjectDatas = new Dictionary<string, JVwithObjectData>();
+    //public Dictionary<string, StageDayData> stageDayDatas = new Dictionary<string, StageDayData>();
+    //public Dictionary<string, StageData> stageDatas = new Dictionary<string, StageData>();
+    //public Dictionary<string, StringData> stringDatas = new Dictionary<string, StringData>();
+
+
+    #region 예전
     public List<MiniGameData> miniGameDatas = new List<MiniGameData>();
     public Dictionary<string, JourneyData> journeyDatas = new Dictionary<string, JourneyData>();
     public Dictionary<string, ItemData> itemDatas = new Dictionary<string, ItemData>();
@@ -12,11 +25,212 @@ public class LoadGameData : Singleton<LoadGameData>
 
     public void LoadCSVDatas()
     {
+        //LoadHeroStateData("CSVData/HeroStateTable");
+        //LoadItemData("CSVData/ItemTable");
+        //LoadJourneyData("CSVData/JourneyTable");
+        //LoadJRwithHeroData("CSVData/JRwithHeroTable");
+        //LoadJStateData("CSVData/JStateTable");
+        //LoadJVCutData("CSVData/JVCutTable");
+        //LoadJVwithObjectData("CSVData/JVwithObjectTable");
+        //LoadStageDayData("CSVData/StageDayTable");
+        //LoadStageData("CSVData/StageTable");
+        //LoadStringData("CSVData/StringTable");
+
+
         LoadMiniGameData("CSVData/MiniGameData");
         LoadJourneyData("CSVData/JourneyData");
         LoadItemData("CSVData/ItemData");
         LoadHeroData("CSVData/HerosData");
     }
+
+    //void LoadHeroStateData(string _path)
+    //{
+    //    heroStateDatas.Clear();
+    //    List<Dictionary<string, object>> data = CSVReader.Read(_path);
+    //    for (int i = 0; i < data.Count; i++)
+    //    {
+    //        HeroStateData heroStateData = new HeroStateData();
+    //        heroStateData.HeroStateID = CSVConvert.ToString(data[i]["HeroStateID"]);
+    //        heroStateData.FirstGive = CSVConvert.ToFloat(data[i]["FirstGive"]);
+    //        heroStateData.Min = CSVConvert.ToFloat(data[i]["Min"]);
+    //        heroStateData.Max = CSVConvert.ToFloat(data[i]["Max"]);
+
+    //        heroStateDatas.Add(heroStateData.HeroStateID, heroStateData);
+    //    }
+    //}
+
+    //void LoadItemData(string _path)
+    //{
+    //    itemDatas.Clear();
+    //    List<Dictionary<string, object>> data = CSVReader.Read(_path);
+    //    for (int i = 0; i < data.Count; i++)
+    //    {
+    //        ItemData itemData = new ItemData();
+    //        itemData.ItemID = CSVConvert.ToString(data[i]["ItemID"]);
+    //        itemData.ItemStringID = CSVConvert.ToString(data[i]["ItemStringID"]);
+    //        itemData.FirstGive = CSVConvert.ToInt(data[i]["FirstGive"]);
+
+    //        itemDatas.Add(itemData.ItemID, itemData);
+    //    }
+    //}
+
+    //void LoadJourneyData(string _path)
+    //{
+    //    journeyDatas.Clear();
+    //    List<Dictionary<string, object>> data = CSVReader.Read(_path);
+    //    for (int i = 0; i < data.Count; i++)
+    //    {
+    //        JourneyData journeyData = new JourneyData();
+    //        journeyData.JourneyID = CSVConvert.ToString(data[i]["JourneyID"]);
+    //        journeyData.StageDayID = CSVConvert.ToString(data[i]["StageDayID"]);
+    //        journeyData.Item1Min = CSVConvert.ToInt(data[i]["Item1Min"]);
+    //        journeyData.Item2Min = CSVConvert.ToInt(data[i]["Item2Min"]);
+    //        journeyData.StageTitleStringID = CSVConvert.ToString(data[i]["StageTitleStringID"]);
+    //        journeyData.JVwithObjectID = CSVConvert.ToString(data[i]["JVwithObjectID"]);
+    //        journeyData.JVCutID = CSVConvert.ToString(data[i]["JVCutID"]);
+    //        journeyData.JRwithHeroID = CSVConvert.ToString(data[i]["JRwithHeroID"]);
+
+    //        journeyDatas.Add(journeyData.JourneyID, journeyData);
+    //    }
+    //}
+
+    //void LoadJRwithHeroData(string _path)
+    //{
+    //    jRwithHeroDatas.Clear();
+    //    List<Dictionary<string, object>> data = CSVReader.Read(_path);
+    //    for (int i = 0; i < data.Count; i++)
+    //    {
+    //        JRwithHeroData jRwithHeroData = new JRwithHeroData();
+    //        jRwithHeroData.JRwithHeroID = CSVConvert.ToString(data[i]["JRwithHeroID"]);
+    //        jRwithHeroData.JRStateID = CSVConvert.ToString(data[i]["JRStateID"]);
+    //        jRwithHeroData.JRanStringID = CSVConvert.ToString(data[i]["JRanStringID"]);
+
+    //        jRwithHeroData.Hero1_Stress = CSVConvert.ToFloat(data[i]["Hero1_Stress"]);
+    //        jRwithHeroData.Hero1_Power = CSVConvert.ToFloat(data[i]["Hero1_Power"]);
+    //        jRwithHeroData.Hero1_Hp = CSVConvert.ToFloat(data[i]["Hero1_Hp"]);
+    //        jRwithHeroData.Hero1_Exp = CSVConvert.ToFloat(data[i]["Hero1_Exp"]);
+
+    //        jRwithHeroData.Hero2_Stress = CSVConvert.ToFloat(data[i]["Hero2_Stress"]);
+    //        jRwithHeroData.Hero2_Power = CSVConvert.ToFloat(data[i]["Hero2_Power"]);
+    //        jRwithHeroData.Hero2_Hp = CSVConvert.ToFloat(data[i]["Hero2_Hp"]);
+    //        jRwithHeroData.Hero2_Exp = CSVConvert.ToFloat(data[i]["Hero2_Exp"]);
+
+    //        jRwithHeroData.Hero3_Stress = CSVConvert.ToFloat(data[i]["Hero3_Stress"]);
+    //        jRwithHeroData.Hero3_Power = CSVConvert.ToFloat(data[i]["Hero3_Power"]);
+    //        jRwithHeroData.Hero3_Hp = CSVConvert.ToFloat(data[i]["Hero3_Hp"]);
+    //        jRwithHeroData.Hero3_Exp = CSVConvert.ToFloat(data[i]["Hero3_Exp"]);
+
+    //        jRwithHeroData.Hero4_Stress = CSVConvert.ToFloat(data[i]["Hero4_Stress"]);
+    //        jRwithHeroData.Hero4_Power = CSVConvert.ToFloat(data[i]["Hero4_Power"]);
+    //        jRwithHeroData.Hero4_Hp = CSVConvert.ToFloat(data[i]["Hero4_Hp"]);
+    //        jRwithHeroData.Hero4_Exp = CSVConvert.ToFloat(data[i]["Hero4_Exp"]);
+
+    //        jRwithHeroDatas.Add(jRwithHeroData.JRwithHeroID, jRwithHeroData);
+    //    }
+    //}
+
+    //void LoadJStateData(string _path)
+    //{
+    //    jStateDatas.Clear();
+    //    List<Dictionary<string, object>> data = CSVReader.Read(_path);
+    //    for (int i = 0; i < data.Count; i++)
+    //    {
+    //        JStateData jStateData = new JStateData();
+    //        jStateData.JStateID = CSVConvert.ToString(data[i]["JStateID"]);
+    //        jStateData.JStateStringID = CSVConvert.ToString(data[i]["JStateStringID"]);
+    //        jStateData.ResultPerfectProb = CSVConvert.ToFloat(data[i]["ResultPerfectProb"]);
+    //        jStateData.ResultNormalProb = CSVConvert.ToFloat(data[i]["ResultNormalProb"]);
+    //        jStateData.ResultFailProb = CSVConvert.ToFloat(data[i]["ResultFailProb"]);
+
+    //        jStateDatas.Add(jStateData.JStateID, jStateData);
+    //    }
+    //}
+
+    //void LoadJVCutData(string _path)
+    //{
+    //    jVCutDatas.Clear();
+    //    List<Dictionary<string, object>> data = CSVReader.Read(_path);
+    //    for (int i = 0; i < data.Count; i++)
+    //    {
+    //        JVCutData jVCutData = new JVCutData();
+    //        jVCutData.JVCutID = CSVConvert.ToString(data[i]["JVCutID"]);
+    //        jVCutData.StateGoodMin = CSVConvert.ToFloat(data[i]["StateGoodMin"]);
+    //        jVCutData.StateNormalMin = CSVConvert.ToFloat(data[i]["StateNormalMin"]);
+    //        jVCutData.StateBadMin = CSVConvert.ToFloat(data[i]["StateBadMin"]);
+    //        jVCutData.StateDangerMin = CSVConvert.ToFloat(data[i]["StateDangerMin"]);
+
+    //        jVCutDatas.Add(jVCutData.JVCutID, jVCutData);
+    //    }
+    //}
+
+    //void LoadJVwithObjectData(string _path)
+    //{
+    //    jVwithObjectDatas.Clear();
+    //    List<Dictionary<string, object>> data = CSVReader.Read(_path);
+    //    for (int i = 0; i < data.Count; i++)
+    //    {
+    //        JVwithObjectData jVwithObjectData = new JVwithObjectData();
+    //        jVwithObjectData.JVwithObjectID = CSVConvert.ToString(data[i]["JVwithObjectID"]);
+    //        jVwithObjectData.MeatValue = CSVConvert.ToFloat(data[i]["MeatValue"]);
+    //        jVwithObjectData.WaterValue = CSVConvert.ToFloat(data[i]["WaterValue"]);
+    //        jVwithObjectData.HubValue = CSVConvert.ToFloat(data[i]["HubValue"]);
+    //        jVwithObjectData.FoodValue = CSVConvert.ToFloat(data[i]["FoodValue"]);
+    //        jVwithObjectData.HStressValue = CSVConvert.ToFloat(data[i]["HStressValue"]);
+    //        jVwithObjectData.HPowerValue = CSVConvert.ToFloat(data[i]["HPowerValue"]);
+
+    //        jVwithObjectDatas.Add(jVwithObjectData.JVwithObjectID, jVwithObjectData);
+    //    }
+    //}
+
+    //void LoadStageDayData(string _path)
+    //{
+    //    stageDayDatas.Clear();
+    //    List<Dictionary<string, object>> data = CSVReader.Read(_path);
+    //    for (int i = 0; i < data.Count; i++)
+    //    {
+    //        StageDayData stageDayData = new StageDayData();
+    //        stageDayData.StageDayID = CSVConvert.ToString(data[i]["StageDayID"]);
+    //        stageDayData.StageID = CSVConvert.ToString(data[i]["StageID"]);
+    //        stageDayData.StageDayStringID = CSVConvert.ToString(data[i]["StageDayStringID"]);
+    //        stageDayData.Time = CSVConvert.ToFloat(data[i]["Time"]);
+    //        stageDayData.HeroBackTime = CSVConvert.ToFloat(data[i]["HeroBackTime"]);
+
+    //        stageDayDatas.Add(stageDayData.StageDayID, stageDayData);
+    //    }
+    //}
+
+    //void LoadStageData(string _path)
+    //{
+    //    stageDatas.Clear();
+    //    List<Dictionary<string, object>> data = CSVReader.Read(_path);
+    //    for (int i = 0; i < data.Count; i++)
+    //    {
+    //        StageData stageData = new StageData();
+    //        stageData.StageID = CSVConvert.ToString(data[i]["StageID"]);
+    //        stageData.StageStringID = CSVConvert.ToString(data[i]["StageStringID"]);
+
+    //        stageDatas.Add(stageData.StageID, stageData);
+    //    }
+    //}
+
+    //void LoadStringData(string _path)
+    //{
+    //    stringDatas.Clear();
+    //    List<Dictionary<string, object>> data = CSVReader.Read(_path);
+    //    for (int i = 0; i < data.Count; i++)
+    //    {
+    //        StringData stringData = new StringData();
+    //        stringData.StringID = CSVConvert.ToString(data[i]["StringID"]);
+    //        stringData.KOR = CSVConvert.ToString(data[i]["KOR"]);
+    //        stringData.ENG = CSVConvert.ToString(data[i]["ENG"]);
+
+    //        stringDatas.Add(stringData.StringID, stringData);
+    //    }
+    //}
+
+
+
+
 
     void LoadMiniGameData(string _path)
     {
@@ -120,6 +334,140 @@ public class LoadGameData : Singleton<LoadGameData>
         string _body = obj.ToString();
         //_body = Regex.Replace(_body, @"[^0-9]", "");
         return float.Parse(_body);
+    }
+    #endregion
+}
+
+//[System.Serializable]
+//public class HeroStateData
+//{
+//    public string HeroStateID;
+//    public float FirstGive;
+//    public float Min;
+//    public float Max;
+//}
+
+//[System.Serializable]
+//public class ItemData
+//{
+//    public string ItemID;
+//    public string ItemStringID;
+//    public int FirstGive;
+//}
+
+//[System.Serializable]
+//public class JourneyData
+//{
+//    public string JourneyID;
+//    public string StageDayID;
+//    public int Item1Min;
+//    public int Item2Min;
+//    public string StageTitleStringID;
+//    public string JVwithObjectID;
+//    public string JVCutID;
+//    public string JRwithHeroID;
+//}
+
+//[System.Serializable]
+//public class JRwithHeroData
+//{
+//    public string JRwithHeroID;
+//    public string JRStateID;
+//    public string JRanStringID;
+//    public float Hero1_Stress;
+//    public float Hero1_Power;
+//    public float Hero1_Hp;
+//    public float Hero1_Exp;
+//    public float Hero2_Stress;
+//    public float Hero2_Power;
+//    public float Hero2_Hp;
+//    public float Hero2_Exp;
+//    public float Hero3_Stress;
+//    public float Hero3_Power;
+//    public float Hero3_Hp;
+//    public float Hero3_Exp;
+//    public float Hero4_Stress;
+//    public float Hero4_Power;
+//    public float Hero4_Hp;
+//    public float Hero4_Exp;
+//}
+
+//[System.Serializable]
+//public class JStateData
+//{
+//    public string JStateID;
+//    public string JStateStringID;
+//    public float ResultPerfectProb;
+//    public float ResultNormalProb;
+//    public float ResultFailProb;
+//}
+
+//[System.Serializable]
+//public class JVCutData
+//{
+//    public string JVCutID;
+//    public float StateGoodMin;
+//    public float StateNormalMin;
+//    public float StateBadMin;
+//    public float StateDangerMin;
+//}
+
+//[System.Serializable]
+//public class JVwithObjectData
+//{
+//    public string JVwithObjectID;
+//    public float MeatValue;
+//    public float WaterValue;
+//    public float HubValue;
+//    public float FoodValue;
+//    public float HStressValue;
+//    public float HPowerValue;
+//}
+
+//[System.Serializable]
+//public class StageDayData
+//{
+//    public string StageDayID;
+//    public string StageID;
+//    public string StageDayStringID;
+//    public float Time;
+//    public float HeroBackTime;
+//}
+
+//[System.Serializable]
+//public class StageData
+//{
+//    public string StageID;
+//    public string StageStringID;
+//}
+
+//[System.Serializable]
+//public class StringData
+//{
+//    public string StringID;
+//    public string KOR;
+//    public string ENG;
+//}
+
+public class CSVConvert
+{
+    public static int ToInt(object _obj)
+    {
+        string body = _obj.ToString();
+        //body = Regex.Replace(body, @"[^0-9]", "");
+        return int.Parse(body);
+    }
+
+    public static float ToFloat(object _obj)
+    {
+        string body = _obj.ToString();
+        //body = Regex.Replace(body, @"[^0-9]", "");
+        return float.Parse(body);
+    }
+
+    public static string ToString(object _obj)
+    {
+        return _obj.ToString();
     }
 }
 
--- HerosManager/Assets/02.Scripts/Utils/CSVReader.cs
+++ HerosManager/Assets/02.Scripts/Utils/CSVReader.cs
@@ -49,34 +49,3 @@ public class CSVReader
         return list;
     }
 }
-
-//public class JsonUtilityHelper
-//{
-//    public static T[] FromJson<T>(string json)
-//    {
-//        string newJson = "";
-//        if (json[0] == '{')
-//        {
-//            newJson = json;
-//        }
-//        else
-//        {
-//            newJson = "{ \"array\": " + json + "}";
-//        }
-//        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
-//        return wrapper.array;
-//    }
-
-//    public static string ToJson<T>(T[] array)
-//    {
-//        Wrapper<T> wrapper = new Wrapper<T>();
-//        wrapper.array = array;
-//        return JsonUtility.ToJson(wrapper);
-//    }
-
-//    [Serializable]
-//    private class Wrapper<T>
-//    {
-//        public T[] array;
-//    }
-//}
