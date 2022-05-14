79906bf5802a6d17fc46154179eec2094d25e7a5
hedra225 <85832675+hedra225@users.noreply.github.com>
2022-05-14 20:33:18 +0900

    수정(도영)
    scene1 : 나무 붕 떠 있는 거 메쉬 수정
    Day Night Cycle maptest 씬에 저장해둠

---
+++ HerosManager/Assets/03.Prefabs/Camp/Objects_re/ObjectsRE/TimeController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeController : MonoBehaviour
+{
+    [SerializeField]
+    private float timeMultipler;
+
+    [SerializeField]
+    private float startHour;
+
+    [SerializeField]
+    private TextMeshProUGUI timeText;
+
+    [SerializeField]
+    private Light sunLight;
+
+    [SerializeField]
+    private float sunriseHour;
+
+    [SerializeField]
+    private float sunsetHour;
+
+    [SerializeField]
+    private Color dayAmbientLight;
+
+    [SerializeField]
+    private Color nightAmbientLight;
+
+    [SerializeField]
+    private AnimationCurve lightChangeCurve;
+
+    [SerializeField]
+    private float maxSunLightIntensity;
+
+    [SerializeField]
+    private Light moonLight;
+
+    [SerializeField]
+    private float maxMoonLightIntensity;
+
+    private DateTime currentTime;
+
+    private TimeSpan sunriseTime;
+
+    private TimeSpan sunsetTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = DateTime.Now.Date + TimeSpan.FromHours(startHour);
+
+        sunriseTime = TimeSpan.FromHours(sunriseHour);
+        sunsetTime = TimeSpan.FromHours(sunsetHour);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTimeofDay();
+        RotateSun();
+        UpdateLightSetting();
+    }
+
+    private void UpdateTimeofDay()
+    {
+        currentTime = currentTime.AddSeconds(Time.deltaTime * timeMultipler);
+
+        if (timeText != null)
+        {
+            timeText.text = currentTime.ToString("HH:mm");
+        }
+    }
+
+    private void RotateSun()
+    {
+        float sunLightRotation;
+        float moonLightRotation;
+
+        if (currentTime.TimeOfDay > sunriseTime && currentTime.TimeOfDay < sunsetTime)
+        {
+            TimeSpan sunriseToSunsetDuration = CalculateTimeDifference(sunriseTime, sunsetTime);
+            TimeSpan timeSinceSunrise = CalculateTimeDifference(sunriseTime, currentTime.TimeOfDay);
+
+            double percentage = timeSinceSunrise.TotalMinutes / sunriseToSunsetDuration.TotalMinutes;
+
+            sunLightRotation = Mathf.Lerp(0, 180, (float)percentage);
+
+            moonLightRotation = Mathf.Lerp(-180, 0, (float)percentage);
+        }
+        else
+        {
+            TimeSpan sunsetTosunriseDuration = CalculateTimeDifference(sunsetTime, sunriseTime);
+            TimeSpan timeSincceSunset = CalculateTimeDifference(sunsetTime, currentTime.TimeOfDay);
+
+            double percentage = timeSincceSunset.TotalMinutes / sunsetTosunriseDuration.TotalMinutes;
+
+            sunLightRotation = Mathf.Lerp(180, 360, (float)percentage);
+
+            moonLightRotation = Mathf.Lerp(-360, -180, (float)percentage);
+        }
+
+        sunLight.transform.rotation = Quaternion.AngleAxis(sunLightRotation, Vector3.right);
+        moonLight.transform.rotation = Quaternion.AngleAxis(moonLightRotation, Vector3.right);
+    }
+
+    private void UpdateLightSetting()
+    {
+        float dotProduct = Vector3.Dot(sunLight.transform.forward, Vector3.down);
+
+        sunLight.intensity = Mathf.Lerp(0, maxSunLightIntensity, lightChangeCurve.Evaluate(dotProduct));
+        moonLight.intensity = Mathf.Lerp(maxMoonLightIntensity, 0, lightChangeCurve.Evaluate(dotProduct));
+
+        RenderSettings.ambientLight = Color.Lerp(nightAmbientLight, dayAmbientLight, lightChangeCurve.Evaluate(dotProduct));
+    }
+    private TimeSpan CalculateTimeDifference(TimeSpan fromTime, TimeSpan toTime)
+    {
+        TimeSpan difference = toTime - fromTime;
+
+        if (difference.TotalSeconds < 0)
+        {
+            difference += TimeSpan.FromHours(24);
+        }
+
+        return difference;
+    }
+}
