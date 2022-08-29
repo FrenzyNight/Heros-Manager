b7febaedd6a5e267a1424f1cca80faea63b92311
KyuHyeon Jeong <83207156+FrenzyNight@users.noreply.github.com>
2022-08-29 19:14:03 +0900

    Update Tutorial
    Script and Json update

--- HerosManager/Assets/TutorialMgr.cs
+++ HerosManager/Assets/TutorialMgr.cs
@@ -8,12 +8,31 @@ public class TutorialMgr : Singleton<TutorialMgr>
     public GameObject tutorialPanelBack;
     public GameObject tutorialDialogPanel;
     public GameObject tutorialWindowPanel;
+    [HideInInspector] public List<TutorialData> tutoDatas;
 
     public void OpenTutorial(string openID)
     {
         tutorialPanelBack.SetActive(true);
+
+        tutoDatas.Clear();
         
-        // 
+        foreach (var obj in LoadGameData.Instance.tutorialDatas)
+        {
+            if (openID == obj.Key)
+            {
+                tutoDatas.Add(obj.Value);
+            }
+        }
+        
+        RunTutorial();
+    }
+
+    public void RunTutorial()
+    {
+        if (tutoDatas == null)
+            return;
+        
+        
     }
 
     public void OpenDialog()
